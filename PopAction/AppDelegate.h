f47c00d50e3e24d7782ec31aa4e92fca8e9d6b52
duks <duks@robooot.com>
2016-09-10 12:20:21 +0800

    Initial Commit

---
+++ PopAction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PopAction
+//
+//  Created by aDu on 16/9/10.
+//  Copyright © 2016年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
